0e022ad129e865990f28fba0ab24488c72ef0ff9
Omkar Adhali <omkar.a2989@gmail.com>
2023-01-10 22:38:53 -0500

    Renaming Components, Adding Notes home page and displaying note cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useMemo } from "react";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { NewNote } from "./new-note";
+import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 import { v4 as uuidV4 } from "uuid";
+import { NoteList } from "./NoteList";
 
 export type Note = {
   id: string;
@@ -60,7 +61,10 @@ function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Hi</h1>} />
+        <Route
+          path="/"
+          element={<NoteList availableTags={tags} notes={notesWithTags} />}
+        />
         <Route
           path="/new"
           element={
